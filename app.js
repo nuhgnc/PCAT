b480e76e6b72648871c5fcfa4a3ecaf33e1f095f
nuhgnc <Nuh16358@gmail.com>
2022-04-18 15:12:20 +0300

    UPDATE

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const path = require('path');
-const ejs = require('ejs');
+const ejs = require('ejs'); //view engine
+
+
 const app = express();
 
 //VİEW ENGİNE
 app.set('view engine', 'ejs');
 
 //MİDDLEWARE
-app.use(express.static('public'));
+app.use(express.static('public')); // Static dosyaları koyacağımız klasörü seçtik
+app.use(express.urlencoded({ extended: true })); // Body parser
+app.use(express.json()); // Body parser
 
 //ROUTES
 app.get('/', (req, res) => {
@@ -19,6 +23,11 @@ app.get('/about', (req, res) => {
 
 app.get('/add', (req, res) => {
   res.render('add');
+});
+
+app.post('/photos', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 const port = 3000;
