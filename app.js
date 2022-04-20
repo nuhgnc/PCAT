2b6da8edb5a0a05ef0a91fcabf290682cb8a3c76
nuhgnc <Nuh16358@gmail.com>
2022-04-20 15:17:31 +0300

    image uplaod

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const path = require('path');
 const ejs = require('ejs');
-const mongoose = require('mongoose')
-const photo = require('./models/Photo') // oluşturduğum schemayı aldım
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 
+const path = require('path');
+const fs = require('fs');
+
+const photo = require('./models/Photo'); // oluşturduğum schemayı aldım
 
 const app = express();
 
-
 //Database connect
-mongoose.connect('mongodb://localhost/pcat-test-db')
+mongoose.connect('mongodb://localhost/pcat-test-db');
 
 //VİEW ENGİNE
 app.set('view engine', 'ejs');
@@ -18,6 +20,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public')); // Static dosyaları koyacağımız klasörü seçtik
 app.use(express.urlencoded({ extended: true })); // Body parser
 app.use(express.json()); // Body parser
+app.use(fileUpload());
 
 //ROUTES
 app.get('/', async (req, res) => {
@@ -33,26 +36,31 @@ app.get('/add', (req, res) => {
   res.render('add');
 });
 
-  app.get('/photo/:photo_id', async (req,res) =>{
-  const foundedPhoto = await photo.findById(req.params.photo_id)
-  res.render('photo', {photo:foundedPhoto})
-})  
-
-/*  app.get('/photo', (req,res) =>{
-  res.render('photo',{photo: {
-    title: 'nuh',
-    description: 'deneme',
-    image: 'asdasd.jps'
-  }})
-})  */
-
-
-app.post('/photos', async (req, res) => {
-  await photo.create(req.body)
-  res.redirect('/');
+app.get('/photo/:photo_id', async (req, res) => {
+  const foundedPhoto = await photo.findById(req.params.photo_id);
+  res.render('photo', { photo: foundedPhoto });
 });
 
+app.post('/photos', async (req, res) => {
+  //Eğer klasör yoksa oluşturacak
+  const uploadDir = 'public/uploads';
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+  // Yükeldiğimiz dosyayı yakalayıp isteiğimiz bilgileri değişkenleri aktarıyoruz
+  let uploadeImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
 
+  // Yakaladığımız dosyayı .mv metodu ile yukarda belirlediğimiz path'a taşıyoruz. Dosya taşıma işlemi sırasında hata olmadı ise req.body ve içerisindeki image'nin dosya yolu ve adıyla beraber database kaydediyoruz
+  uploadeImage.mv(uploadPath, async (err) => {
+    if (err) console.log(err);    // Bu kısımda önemli olan add.ejs'nin içerisine form elemanı olarak encType="multipart/form-data" atribute eklemek
+    await photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadeImage.name,
+    });
+  });
+  res.redirect('/');
+});
 
 const port = 3000;
 app.listen(port, () => {
